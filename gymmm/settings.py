26a2c559f7cc1ae047be1fa3166f3c069191f5b5
Dhyan V Shetty <dhyanshetty777@gmail.com>
2026-02-16 11:24:07 +0530

    rr

--- gymmm/settings.py
+++ gymmm/settings.py
@@ -178,7 +178,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + ["authorization"]
 CORS_ALLOWED_ORIGINS = [
      "http://localhost:5173",
      "http://127.0.0.1:5173",
-     "https://gymsaasdhyan.netlify.app/",
+     "https://gymsaasdhyan.netlify.app",
  ]
 
 CORS_ALLOW_CREDENTIALS = True
