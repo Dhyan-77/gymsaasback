1877a370c60772a1f65ef35898b0824c331eae29
Dhyan V Shetty <dhyanshetty777@gmail.com>
2026-02-23 12:44:11 +0530

    eeee

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import SignupView, MeView
+from .views import SignupView, MeView, SubscriptionStatusView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("me/", MeView.as_view(), name="me"),
+    path("me/", SubscriptionStatusView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny,IsAuthenticated
-
+from billing.models import OwnerSubscription
+from django.utils import timezone
 from .serializers import signupserializers
 
 
@@ -39,4 +40,31 @@ class MeView(APIView):
             "id": str(user.id),
             "email": user.email,
             "username": user.username,
+        })
+
+
+
+class SubscriptionStatusView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        sub = (
+            OwnerSubscription.objects
+            .filter(owner=request.user)
+            .order_by("-created_at")
+            .first()
+        )
+
+        if not sub:
+            return Response({"status": "none"})
+
+        days_remaining = None
+        if sub.current_end:
+            delta = sub.current_end - timezone.now()
+            days_remaining = max(delta.days, 0)
+
+        return Response({
+            "status": sub.status,
+            "current_end": sub.current_end,
+            "days_remaining": days_remaining,
         })
